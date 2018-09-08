94b2dc8545834c6a65534dedf1879c18d5d89497
Prithvi Thakur <prithvi.thakur93@gmail.com>
2018-09-08 19:03:58 -0400

    lab01

--- Clasp410/Lab01/src/forest-fire-model.py
+++ Clasp410/Lab01/src/forest-fire-model.py
@@ -4,7 +4,7 @@
 #
 #   Author: Prithvi Thakur
 #   Written in: Python v3.6.6
-#   Last Modified: 
+#   Last Modified: 09-08-2018
 #
 #   Clasp 410 Lab 1: In a two dimensional forest
 #                    with some trees and some bare
@@ -25,102 +25,138 @@ def grid(Nt, Nx, Ny):
 
 # Define probabilities
 class probabilities:
-    start_fire = 0.25
-    catch_fire = 0.3
-    bare_patch = 0.225
-    ignite = 0.5
+    start_fire = 0.0025   # Probability that each cell is on fire   
+    catch_fire = 1    # Probability that a neighboring node catches fire
+    bare_patch = 0.225  # Probability that there is a bare patch 
 
-# Domain size and timesteps
-Nx = 100
-Ny = 100
-Nt = 20
-Grid = grid(Nt, Nx, Ny)
+def initialize(Grid, Nx, Ny, prob):
+    # Initialize the domain
+    #   1 = bare ground
+    #   2 = forest
+    #   3 = fire
+    # Edges are bare ground by default
+    for i in range(1,Nx-1):
+        for j in range(1,Ny-1):
+            # initialize trees in forest, default is bare ground
+            if np.random.random() > prob.bare_patch:
+                Grid[0, i,j] = 2
 
-# Initialize Probabilities
-prob = probabilities()
+    return Grid
 
-# Initialize the domain
-#   1 = bare ground
-#   2 = forest
-# Edges are bare ground by default
-for i in range(1,Nx-1):
-    for j in range(1,Ny-1):
-        # initialize trees in forest, default is bare ground
-        if np.random.random() > prob.bare_patch:
-            Grid[0, i,j] = 2
-
-# Initial grid for plotting
-Grid_initial = Grid[0,:,:]
-
-# Initial forest fire
-for i in range(1,Nx-1):
-    for j in range(1,Ny-1):
-        if np.random.random() < prob.catch_fire:
-            if Grid[0, i,j] == 2:
-                Grid[0, i,j] = 3
+def initial_forest_fire(Grid, Nx, Ny, prob):
+    # Assign initial forest fire cells
+    for i in range(1,Nx-1):
+        for j in range(1,Ny-1):
+            if np.random.random() < prob.start_fire:
+                # If grid is forest and probability is favorable, then start fire
+                if Grid[0, i,j] == 2:
+                    Grid[0, i,j] = 3
+    return Grid
 
 
-# Spread the fire
-for t in range(Nt-1):
+def spread_fire(Grid, Nx, Ny, prob):
+    # Spread the fire
     for i in range(1, Nx-1):
         for j in range(1, Ny-1):
             
-            if Grid[t,i,j] == 3:        # If the current cell is on fire
+            if Grid[i,j] == 3:        # If the current cell is on fire
                 
-                if Grid[t,i,j+1] == 2:  # If neighbors are trees
-                    if np.random.random() < prob.ignite:
-                        Grid[t,i,j+1] = 3
+                if Grid[i,j+1] == 2:  # If neighbors are trees, check each neighbor
+                    if np.random.random() < prob.catch_fire:
+                        Grid[i,j+1] = 3
 
-                if Grid[t,i+1,j] == 2:
-                    if np.random.random() < prob.ignite:
-                        Grid[t,i+1,j] = 3
+                if Grid[i+1,j] == 2:
+                    if np.random.random() < prob.catch_fire:
+                        Grid[i+1,j] = 3
 
-                if Grid[t,i-1,j] == 2:
-                    if np.random.random() < prob.ignite:
-                        Grid[t,i-1,j] = 3
+                if Grid[i-1,j] == 2:
+                    if np.random.random() < prob.catch_fire:
+                        Grid[i-1,j] = 3
 
-                if Grid[t,i,j-1] == 2:
-                    if np.random.random() < prob.ignite:
-                        Grid[t,i,j-1] = 3
+                if Grid[i,j-1] == 2:
+                    if np.random.random() < prob.catch_fire:
+                        Grid[i,j-1] = 3
              
                 # Extinguish initial fire
-                Grid[t,i,j] = 1   
-
-    # Assign previous solution to the next iteration
-    Grid[t+1,:,:] = Grid[t,:,:]
+                Grid[i,j] = 1   
+    return Grid
 
 
-# Animate the spread
+# Main function
+def main():
+    # Domain size and timesteps
+    Nx = 250     # X dimension
+    Ny = 250     # Y dimension
+    Nt = 60     # No. of timesteps
+    
+    # Iterators
+    k = 1       # Check when no more trees on fire 
+    t = 0       # Iterator
 
-def init():
-    return [im]
+    # Initialize grid
+    Grid = grid(Nt, Nx, Ny)
+    
+    # Initialize Probabilities
+    prob = probabilities()
 
-def frame(i):
-    a = im.get_array()
-    a = Grid[i,:,:]
-    im.set_array(a)
+    # Initialize the domain
+    Grid = initialize(Grid, Nx, Ny, prob)
 
-    return [im]
+    # Initial grid for plotting
+    Grid_initial = Grid[0,:,:]
+
+    # Initial forest fire
+    Grid = initial_forest_fire(Grid, Nx, Ny, prob)
+
+    # Time loop
+    while k != 0:
+
+        # Spread the fire
+        Grid[t,:,:] = spread_fire(Grid[t,:,:], Nx, Ny, prob)
+        
+        # Assign previous solution to the next iteration
+        Grid[t+1,:,:] = Grid[t,:,:]
+
+        # Count the number of trees on fire, i.e., the value is 3
+        unique, counts =  np.unique(Grid[t,:,:], return_counts = True)
+        
+        no_fire_cells = counts[np.where(unique == 3)[0]]
+        
+        # Check if no more trees are on fire
+        if no_fire_cells.size == 0:
+            k = 0
+        
+        #  print(Grid[t,:,:])
+        t = t+1
+
+    # Remove extra allocations from Grid
+    Grid = Grid[0:t, :,:]
+    print(t)
+    # Animation functions
+    def init():
+        return [im]
+
+    def frame(i):
+        a = im.get_array()
+        a = Grid[i,:,:]
+        im.set_array(a)
+        return [im]
+
+    # Plot and animate
+    fig = plt.figure()
+    ax = plt.axes(xlim=(0,Nx), ylim=(0,Ny))
+    ax.set_xlabel("X axis")
+    ax.set_ylabel("Y axis")
+    ax.set_title("Forest fire spread")
+
+    im = ax.imshow(Grid[0,:,:], extent = [0,Nx,0,Ny], interpolation='bilinear')
+
+    anim = animation.FuncAnimation(fig, frame, init_func=init, frames = t,
+                                   interval=500, blit=True)
+    plt.show()
+    anim
 
 
-#  def anim(Grid):
-
-#  Nx = len(Grid[0,:,0])
-#    Ny = len(Grid[0,0,:])
-#    Nt = len(Grid[:,0,0])
-
-fig = plt.figure()
-ax = plt.axes(xlim=(0,Nx), ylim=(0,Ny))
-ax.set_xlabel("X axis")
-ax.set_ylabel("Y axis")
-ax.set_title("Forest fire spread")
-
-im = ax.imshow(Grid[0,:,:], extent = [0,Nx,0,Ny], interpolation='bilinear')
-
-anim = animation.FuncAnimation(fig, frame, init_func=init, frames = Nt,
-                               interval=500, blit=True)
-
-plt.show()
-
-
-anim
+# Execute main function
+if __name__ =="__main__":
+    main()
