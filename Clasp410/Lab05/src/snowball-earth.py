7de2dbf0f82a9da73805101dda931e6f59752413
Prithvi Thakur <prithvi.thakur93@gmail.com>
2018-10-31 09:31:46 -0400

    add figures for lab 04

---
+++ Clasp410/Lab05/src/snowball-earth.py
@@ -0,0 +1,23 @@
+######################################################
+#
+#   SNOWBALL EARTH
+#
+#   Author: Prithvi Thakur
+#   Written in: Python v3.6.6
+#   Last Modified: 10-30-2018
+#
+#   Clasp 410 Lab 5: Snowball earth
+#                    
+######################################################
+
+
+# Import modules
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+
+# Path to save figures
+path = '/Volumes/GoogleDrive/My Drive/Courses/coursework/Clasp410/Lab05/figs/'
+
+
+
